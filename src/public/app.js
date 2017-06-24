f4a9a3938f4a4f7cf0d8c2cb3631e7fddb3612fc
שקד חזון <hazons@post.bgu.ac.il>
2017-06-24 21:47:05 +0300

    Added views

--- src/public/app.js
+++ src/public/app.js
@@ -5,13 +5,30 @@ app.config(function (localStorageServiceProvider)
     localStorageServiceProvider.setPrefix('node_angular_App');
 });
 
+app.config(['$locationProvider', function ($locationProvider)
+{
+    $locationProvider.hashPrefix('');
+}]);
+
+app.config(['$routeProvider', function ($routeProvider)
+{
+    $routeProvider.when("/albums", {
+        templateUrl: "views/Albums.html",
+        controller: "testController"
+    }).when("/users", {
+        templateUrl: "views/Users.html",
+        controller: 'testController'
+    }).otherwise({
+        redirect: '/'
+    });
+}]);
 app.controller('testController', ['RestClient', function (RestClient)
 {
     let vm = this;
     vm.name = 'Working!';
     RestClient.getSomething().then(function (data)
     {
-        vm.hottest = data;
+        vm.genres = data;
     }).catch(function (err)
     {
         vm.hottest = err;
@@ -25,11 +42,7 @@ app.factory('RestClient', ['$http', function ($http)
     {
         return $http.get('/albums/genres').then(function (response)
         {
-            return new Promise(function (resolve, reject)
-            {
-                resolve(response.data);
-            });
-            // return Promise.resolve(response.data);
+            return Promise.resolve(response.data);
         }).catch(function (err)
         {
             return Promise.reject(err);
